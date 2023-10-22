0fbe5b3bb05d1199d0f286885a275c230a846afb
Huntshi <tribottetimeo@gmail.com>
2023-10-22 20:46:44 +0200

    page detaille à faire styliser + debut ajouter logement

---
+++ Pages/ajouter_logement.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un logement</title>
+</head>
+<body>
+    <header>
+        <img src="/Ressources/Images/logo.svg">
+        <h2>Loc'Breizh</h2>
+
+        <img src="/Ressources/Images/filtre.svg">
+
+        <input id="searchbar" type="text" name="search">
+        <img src="/Ressources/Images/loupe.svg">
+
+        <img src="/Ressources/Images/booklet-fill 1.svg">
+        <h4>Accèder à mes réservation</h4>
+
+
+        <img src="/Ressources/Images/message.svg">
+        <img src="/Ressources/Images/compte.svg">
+        <hr>
+    </header>
+
+    <div class='banniere'>
+        <img src=''>
+        <h1>Remplir la fiche logement</h1>
+    </div>
+
+    <main>
+
+    </main>
+
+    <footer class="mt-4 container-fluid">
+        <div class="mt-4 column">
+            <div class="col-12 text-center">
+                <a class="col-2" href="mailto:locbreizh@alaizbreizh.com">locbreizh@alaizbreizh.com</a>
+                <a class="offset-md-1 col-2" href="tel:+33623455689">(+33) 6 23 45 56 89</a>
+                <a class="offset-md-1 col-1" href="connexion.html"><img src="svg/instagram.svg"> @LocBreizh</a>
+                <a class="offset-md-1 col-1" href="connexion.html"><img src="svg/facebook.svg"> @LocBreizh</a>
+            </div>
+            <hr>
+            <div class="logo">
+                <img src="/Ressources/Images/logo2.svg">
+            </div>
+            <div class="offs3et-md-1 col-10 mt-4 text-center row">
+                <p class="offset-md-1 col-2">©2023 Loc’Breizh</p>
+                <p class="offset-md-1 col-3" style="text-decoration: underline;"><a href="connexion.html">Conditions générales</a></p>
+                <p class="offset-md-1 col-4">Développé par <a href="connexion.html" style="text-decoration: underline;">7ème sens</a></p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
