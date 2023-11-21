0ae1477c71e1c2d3b3162b991fe2f709c198183d
FILAUX NATHAN <nfilaux@mx21>
2023-11-21 12:04:46 +0100

    debut profil

---
+++ Page_Html/Compte/consulter_profil_client.php
@@ -0,0 +1,28 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon profil</title>
+</head>
+<body>
+    <header>
+        <h1>Mon compte</h1>
+    </header>
+    
+    <main>
+        <div>
+            <p>Prénom</p>
+            <p>Nom</p>
+            <p>Pseudo</p>
+            <p>E-mail</p>
+            <p>Téléphone</p>
+            <p>Mot de passe</p>
+            <p>Date de naissance</p>
+            <p>Adresse</p>
+        </div>
+    </main>
+    
+</body>
+</html>
