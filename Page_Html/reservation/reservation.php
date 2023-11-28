317380e4c542ffadb3b91c671437e7c786f58d8c
LECONTE ESTEBAN <esleconte@mx21>
2023-11-28 12:11:52 +0100

    reservation payement css

--- Page_Html/reservation/reservation.php
+++ Page_Html/reservation/reservation.php
@@ -48,27 +48,28 @@ $photo = $stmt->fetch();
         </div>
     </header>
 
-    <main>
-        <h1>Paiement de la reservation</h1>
+    <main class="MainReser">
+        <div class="headtablo"> 
+            <h1>Paiement de la reservation</h1>
+        </div>
+       
         <div id="paypal-button-container"></div>
         <script src="https://www.paypal.com/sdk/js?client-id=AZ9f4oyPDCMS1YixHmqPR4wPGz2OM5CC59wpo0KTCHc2jaOMYZivqTjz-Wj52tAprCRF5PkLZD1dnn9s&currency=EUR"></script>
         <script src="paypal.js"></script>
     </main>
     <footer>
-        <div>   
-            <div>
+            <div class="tfooter">
                 <p><a href="mailto:locbreizh@alaizbreizh.com">locbreizh@alaizbreizh.com</a></p>
                 <p><a href="tel:+33623455689">(+33) 6 23 45 56 89</a></p>
-                <p><a href="connexion.html"><img src="../svg/instagram.svg">  @LocBreizh</a></p>
-                <p><a href="connexion.html"><img src="../svg/facebook.svg">  @LocBreizh</a></p>
+                <a class="margintb" href="connexion.html"><img src="../svg/instagram.svg">  <p>@LocBreizh</p></a>
+                <a  class="margintb" href="connexion.html"><img src="../svg/facebook.svg">  <p>@LocBreizh</p></a>
             </div>
             <hr>  
-            <div>
+            <div class="bfooter">
                 <p>©2023 Loc’Breizh</p>
-                <p><a href="connexion.html">Conditions générales</a></p>
+                <p style="text-decoration: underline;"><a href="connexion.html">Conditions générales</a></p>
                 <p>Développé par <a href="connexion.html" style="text-decoration: underline;">7ème sens</a></p>
             </div>
-        </div>
     </footer>
 </body>
 
