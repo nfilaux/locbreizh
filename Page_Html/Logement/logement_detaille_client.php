694c5b5cf3c0f51d50423999904ab2e3c62d6db2
FILAUX NATHAN <nfilaux@mx21>
2023-12-20 15:21:41 +0100

    page css + fix mauvais merge

--- Page_Html/Logement/logement_detaille_client.php
+++ Page_Html/Logement/logement_detaille_client.php
@@ -136,11 +136,7 @@ $photo = $stmt->fetch();
                             <p class="dateresa"></p>
                             <p class="dateresa"></p>
                         </div>
-<<<<<<< HEAD
                         <form action="../demande_devis/demande_devis.php?logement=<?php echo $_GET['logement']; ?>" method="post">
-=======
-                        <form class="center" action="../demande_devis/demande_devis.php?logement=<?php echo $_GET['logement']; ?>" method="post">
->>>>>>> 021df732112c025f25cd155458644d174f3ab4d2
                             <input type='hidden' name="arrive" id="arrive" value="">
                             <input type='hidden' name="depart" id="depart" value="">
                             <button class="btn-demlog" type="submit">Demander un devis</button>
