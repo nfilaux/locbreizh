f951fc89f2d53c02babbff0afcae782d9d08490d
FILAUX NATHAN <nfilaux@mx21>
2024-01-26 15:00:03 +0100

    test script

--- Page_Html/Accueil/Tableau_de_bord.php
+++ Page_Html/Accueil/Tableau_de_bord.php
@@ -23,6 +23,435 @@ $plageDispo = [];
 ?>
 
 <script>
+    //recupération des element du html qu'on vas remplir d'information
+dateActuelle = [];
+baliseJour =[];
+precedentSuivant = [];
+datesPlage = [];
+boutonsDates = [];
+prixSejour = [];
+
+//création de dates qui vont êtres utilisé pour le premier et deuxieme calendrier
+date = [];
+anneeActuelle = [];
+moisActuel = [];
+date2 = [];
+anneeActuelle2 = [];
+moisActuel2 = [];
+
+//constante pour les mois de l'année
+const tabMois = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
+//tableaux qui gardent la liste des messages et de plages d'indisponibilitées
+var tabDispo = [];
+var tabPrix = [];
+var classeDispo = [];
+var tabIndispo = [];
+var tabRaison = [];
+var classeIndispo = [];
+
+//tableau des calendriers du code HTML
+calendrier = [];
+
+//instanciation du debut et de la fin de la plage
+premierID = [];
+dernierID = [];
+
+//classe des jours normaux
+classeNormale = [];
+
+//prix des plages sélectionner
+prixPlage = [];
+
+function instancier(id, nbCache){
+    //recupération des element du html qu'on vas remplir d'information
+    calendrier[id] = document.getElementById("calendrier" + id);
+    dateActuelle[id] = calendrier[id].querySelectorAll(".date_actuelle");
+    baliseJour[id] = calendrier[id].querySelectorAll(".jours");
+    precedentSuivant[id] = calendrier[id].querySelectorAll(".fleches svg");
+    datesPlage[id] = [document.querySelectorAll("#datesPlage .dateresa")[id*2], ''];
+    datesPlage[id][1] = document.querySelectorAll("#datesPlage .dateresa")[id*2+1];
+    if (nbCache == 4){
+        boutonsDates[id] = [document.querySelectorAll(".jesuiscache")[id*4], '', '', ''];
+        boutonsDates[id][1] = (document.querySelectorAll(".jesuiscache")[id*4+1]);
+        boutonsDates[id][2] = (document.querySelectorAll(".jesuiscache")[id*4+2]);
+        boutonsDates[id][3] = (document.querySelectorAll(".jesuiscache")[id*4+3]);
+    }
+    else{
+        boutonsDates[id] = [document.querySelectorAll(".jesuiscache")[id*2], ''];
+        boutonsDates[id][1] = (document.querySelectorAll(".jesuiscache")[id*2+1]);
+    }
+    prixSejour[id] = document.querySelectorAll(".nuit")[id];
+
+    //création de dates qui vont êtres utilisé pour le premier et deuxieme calendrier
+    date[id] = new Date();
+    anneeActuelle[id] = date[id].getFullYear();
+    moisActuel[id] = date[id].getMonth();
+    date2[id] = new Date(anneeActuelle[id], moisActuel[id] + 1);
+    anneeActuelle2[id] = date2[id].getFullYear();
+    moisActuel2[id] = date2[id].getMonth();
+
+    //instanciation du debut et de la fin de la plage
+    premierID[id] = "";
+    dernierID[id] = "";
+
+    //classe des jours normaux
+    classeNormale[id] = "";
+
+    //permet de changer les dates des calendrier et de les actualiser quand on appuie sur les flèches
+    precedentSuivant[id].forEach(element => {
+        element.addEventListener("click", () => {
+            //change les mois en fonction de si on appuie sur precedent ou suivant
+            if (element.id === "precedent") {
+                moisActuel[id] = moisActuel[id] - 1;
+                moisActuel2[id] = moisActuel2[id] - 1;
+            }
+            else {
+                moisActuel[id] = moisActuel[id] + 1;
+                moisActuel2[id] = moisActuel2[id] + 1;
+            }
+            //recrée toutes les dates en fonctions des nouveaux mois et affiche les calendriers actualisés
+            date[id] = new Date(anneeActuelle[id], moisActuel[id]);
+            anneeActuelle[id] = date[id].getFullYear();
+            moisActuel[id] = date[id].getMonth();
+            date2[id] = new Date(anneeActuelle2[id], moisActuel2[id]);
+            anneeActuelle2[id] = date2[id].getFullYear();
+            moisActuel2[id] = date2[id].getMonth();
+            afficherCalendrier(classeNormale[id], id);
+            if (classeDispo[id]){
+                afficherPlages(tabDispo[id], classeDispo[id], tabPrix[id], "D", id);
+            }
+            if (classeIndispo[id]){
+                afficherPlages(tabIndispo[id], classeIndispo[id], tabRaison[id], "I", id);
+            }
+            if (premierID[id] !== ""){
+                selection(premierID[id], dernierID[id], id);
+            }
+        })
+    });
+}
+
+//fonction pour actualiser le calendrier en fonction des dates
+function afficherCalendrier(classe, id) {
+    classeNormale[id] = classe;
+    //création des dates importantes du calendrier de gauche
+    premierJourMois = new Date(anneeActuelle[id], moisActuel[id], 0).getDay();
+    derniereDateMois = new Date(anneeActuelle[id], moisActuel[id] + 1, 0).getDate();
+    derniereJourMois = new Date(anneeActuelle[id], moisActuel[id], derniereDateMois - 1).getDay();
+    derniereDateMoisAvant = new Date(anneeActuelle[id], moisActuel[id], 0).getDate();
+    texteListe = "";
+
+    //création des dates importantes du calendrier de droite
+    premierJourMois2 = new Date(anneeActuelle2[id], moisActuel2[id], 0).getDay();
+    derniereDateMois2 = new Date(anneeActuelle2[id], moisActuel2[id] + 1, 0).getDate();
+    derniereJourMois2 = new Date(anneeActuelle2[id], moisActuel2[id], derniereDateMois2 - 1).getDay();
+    derniereDateMoisAvant2 = new Date(anneeActuelle2[id], moisActuel2[id], 0).getDate();
+    texteListe2 = "";
+
+    //création de variables qui permettent de créer le calendrier
+    nbJours = 0;
+    k = 0;
+
+    //création du calendrier de gauche
+    for (i = premierJourMois - 1; i >= 0; i--) {
+        texteListe += '<li class="inactif">' + (derniereDateMoisAvant - i) + '</li>';
+        nbJours++;
+    }
+
+    for (i = 1; i <= derniereDateMois; i++) {
+        idJour = (moisActuel[id]+ 1) + '/' + (k + 1) + '/' + anneeActuelle[id];
+        texteListe += '<li onclick="changerJour(this.id, ' + id + ')" id=' + id + "," + idJour + ' class="' + classe + '">' + i + '</li>';
+        k++;
+        nbJours++;
+    }
+
+
+    for (i = derniereJourMois; nbJours < 42; i++) {
+        texteListe += '<li class="inactif">' + (i - derniereJourMois + 1) + '</li>';
+        nbJours++;
+    }
+
+
+    if (dateActuelle[id].length == 2){
+        //création du calendrier de droite
+        for (i = premierJourMois2 - 1; i >= 0; i--) {
+            texteListe2 += '<li class="inactif">' + (derniereDateMoisAvant2 - i) + '</li>';
+            nbJours++;
+        }
+
+        for (i = 1; i <= derniereDateMois2; i++) {
+            idJour = (moisActuel2[id] + 1) + '/' + (k + 1 - derniereDateMois) + '/' + anneeActuelle2[id];
+            texteListe2 += '<li onclick="changerJour(this.id, ' + id + ')" id=' + id + "," + idJour + ' class="' + classe + '">' + i + '</li>';
+            k++;
+            nbJours++;
+        }
+
+        for (i = derniereJourMois2; nbJours < 84; i++) {
+            texteListe2 += '<li class="inactif">' + (i - derniereJourMois2 + 1) + '</li>';
+            nbJours++;
+        }
+        baliseJour[id][1].innerHTML = texteListe2;
+        dateActuelle[id][1].innerHTML = (tabMois[moisActuel2[id]] + ' ' + anneeActuelle2[id]);
+    }
+
+    //affiche les mois/années des calendriers
+    dateActuelle[id][0].innerHTML = (tabMois[moisActuel[id]] + ' ' + anneeActuelle[id]);
+    baliseJour[id][0].innerHTML = texteListe;
+}
+
+//permet de créer une selection
+function selection(debut, fin, id) {
+    changerJour(debut, id);
+    if (fin !== debut){
+        changerJour(fin, id);
+    }
+}
+
+//permet de changer les styles des jours en fonction de la selection
+function changerJour(elem, id) {
+    //cas ou l'id n'ais pas sur le calendrier
+    if (document.getElementById(elem) && document.getElementById(elem).className !== "inactif"){
+        //recupération de l'élément et réinitialistaion du calendrier
+        element = document.getElementById(elem);
+        nbActif = calendrier[id].getElementsByClassName("actif").length;
+        entreDeux = calendrier[id].getElementsByClassName("entreDeux");
+        nbEntreDeux = entreDeux.length;
+        for (i = 0; i < nbEntreDeux; i++) {
+            entreDeux[0].className = "normal";
+        }
+        //remet les plages
+        if (tabDispo[id][0]){
+            afficherPlages(tabDispo[id], classeDispo[id], tabPrix[id], "D", id);
+        }
+        if (tabIndispo[id][0]){
+            afficherPlages(tabIndispo[id], classeIndispo[id], tabRaison[id], "I", id);
+        }
+        //cas où l'élément n'est pas une date de début ou de fin de palge
+        if (element.className !== "actif") {
+            //cas ou il n'y as aucune dates de sélectionner
+            if (nbActif == 0) {
+                premierID[id] = element.id;
+                dernierID[id] = element.id;
+                element.className = "actif";
+            }
+            //cas ou il il y a une ou deux dates de sélectionner
+            else {
+                let dateElem = new Date(element.id.split(',')[1]).getTime();
+                let datePremier = new Date(premierID[id].split(',')[1]).getTime();
+                let dateDernier = new Date(dernierID[id].split(',')[1]).getTime();
+                milieu = (dateDernier + datePremier) / 2;
+                //détermine si le nouveau jour seras le début ou la fin de la plage
+                if (dateElem < milieu) {
+                    if (nbActif > 1) {
+                        if (tabDispo[id].includes(premierID[id]) ){
+                            document.getElementById(premierID[id]).className = classeDispo[id];
+                        }
+                        else if(tabIndispo[id].includes(premierID[id])){
+                            document.getElementById(premierID[id]).className = classeIndispo[id];
+                        }
+                        else{
+                            document.getElementById(premierID[id]).className = "normal";
+                        }
+                    }
+                    premierID[id] = element.id;
+                    element.className = "actif";
+                }
+                else {
+                    if (nbActif > 1) {
+                        if (tabDispo[id].includes(dernierID[id]) ){
+                            document.getElementById(dernierID[id]).className = classeDispo[id];
+                        }
+                        else if(tabIndispo[id].includes(dernierID[id])){
+                            document.getElementById(dernierID[id]).className = classeIndispo[id];
+                        }
+                        else{
+                            document.getElementById(dernierID[id]).className = "normal";
+                        }
+                    }
+                    dernierID[id] = element.id;
+                    element.className = "actif";
+                }
+                //active la zone de selection entre les deux dates
+                datePremier = new Date(premierID[id].split(',')[1]).getTime();
+                dateDernier = new Date(dernierID[id].split(',')[1]).getTime();
+                listeJours = calendrier[id].querySelectorAll(".jours li");
+                inactif = false;
+                fini = false;
+                loop = 0;
+                while (!inactif && !fini){
+                    jour = listeJours[loop];
+                    if (!jour){
+                        fini = true;
+                    }
+                    if (!fini){
+                        let dateJour = new Date(jour.id.split(',')[1]).getTime();
+                        if (dateJour < dateDernier && dateJour > datePremier) {
+                            if (jour.className !== "inactif"){
+                                jour.className = "entreDeux";
+                            }
+                            else{
+                                inactif = true;
+                            }
+                            
+                        }
+                        else if (dateJour === dateDernier){
+                            fini = true;
+                        }
+                    }
+                    loop++;
+                }
+                if (inactif){
+                    nbEntreDeux = calendrier[id].getElementsByClassName("entreDeux").length;
+                    JourAChanger = document.getElementById(dernierID[id]);
+                    JourAChanger.className = classeDispo[id];
+                    if (nbEntreDeux > 0){
+                        dernierID[id] = listeJours[loop-2].id;
+                        document.getElementById(dernierID[id]).className = "actif";
+                        dernierID[id].className = "actif";
+                    }
+                    else{
+                        dernierID[id] = premierID[id];
+                    }
+                }
+            }
+        }
+        //désactive le jour si on clique dessus
+        else if (element.className === "actif") {
+            if (nbActif == 2) {
+                if (element.id === premierID[id]) {
+                    premierID[id] = dernierID[id];
+                }
+                else {
+                    dernierID[id] = premierID[id];
+                }
+            }
+            if (tabDispo[id].includes(element.id) ){
+                element.className = classeDispo[id];
+            }
+            else if(tabIndispo[id].includes(element.id)){
+                element.className = classeIndispo[id];
+            }
+            else{
+                element.className = "normal";
+            }
+        }
+        nbActif = document.getElementsByClassName("actif").length;
+        if (nbActif == 0){
+            premierID[id] = "";
+            dernierID[id] = "";
+        }
+        changerDates(id);
+    }
+}
+
+//change les dates se trouvant à coté du calendrier et dans le formulaire pour demander un devis
+function changerDates(id) {
+    listeActif = calendrier[id].getElementsByClassName("actif");
+    listeEntreDeux = calendrier[id].getElementsByClassName("entreDeux");
+    pIdDate = premierID[id].split(',')[1];
+    dIdDate = dernierID[id].split(',')[1];
+    if (listeActif.length !== 0){
+        //change format du premier ID
+        if (pIdDate.split('/')[1].length == 1) {
+            newPId = '0' + pIdDate.split('/')[1];
+        }
+        else {
+            newPId = pIdDate.split('/')[1];
+        }
+        if (pIdDate.split('/')[0].length == 1) {
+            newPId += '/0' + pIdDate.split('/')[0] + '/' + pIdDate.split('/')[2];
+        }
+        else {
+            newPId += '/' + pIdDate.split('/')[0] + '/' + pIdDate.split('/')[2];
+        }
+
+        //change format du dernier ID
+        if (dIdDate.split('/')[1].length == 1) {
+            newDId = '0' + dIdDate.split('/')[1];
+        }
+        else {
+            newDId = dIdDate.split('/')[1];
+        }
+        if (dIdDate.split('/')[0].length == 1) {
+            newDId += '/0' + dIdDate.split('/')[0] + '/' + dIdDate.split('/')[2];
+        }
+        else {
+            newDId += '/' + dIdDate.split('/')[0] + '/' + dIdDate.split('/')[2];
+        }
+        if (datesPlage[id][0]) {
+            //envoie les dates dans le HTML pour l'affichage et pour l'envoie de devis
+            datesPlage[id][0].innerHTML = "<p>Arrivée</p><p>" + newPId + "</p>";
+            datesPlage[id][1].innerHTML = "<p>Départ</p><p>" + newDId + "</p>";
+        }
+        if (boutonsDates[id][0]) {
+            newPId = newPId.split('/')[2] + "-" + newPId.split('/')[1] + "-" + newPId.split('/')[0];
+            newDId = newDId.split('/')[2] + "-" + newDId.split('/')[1] + "-" + newDId.split('/')[0];
+            for (i=0; i < boutonsDates[id].length; i+=2){
+                boutonsDates[id][i].value = newPId;
+                boutonsDates[id][i+1].value = newDId;
+            }
+        }
+        if (prixSejour[id]){
+            prixPlage[id] = 0;
+            for (i=0; i<listeActif.length; i++){
+                prixPlage[id] += parseInt(tabPrix[id][tabDispo[id].indexOf(listeActif[i].id.split(',')[1])]);
+            }
+            for (i=0; i<listeEntreDeux.length; i++){
+                prixPlage[id] += parseInt(tabPrix[id][tabDispo[id].indexOf(listeEntreDeux[i].id.split(',')[1])]);
+            }
+            prixSejour[id].innerHTML = prixPlage[id] + "€ pour les nuits";
+        }
+    }
+    else{
+        if (datesPlage[id][0]) {
+            //envoie les dates dans le HTML pour l'affichage et pour l'envoie de devis
+            datesPlage[id][0].innerHTML = "<p>Arrivée</p><p>" + " ../../.... " + "</p>";
+            datesPlage[id][1].innerHTML = "<p>Départ</p><p>" + " ../../.... " + "</p>";
+        }
+        if (boutonsDates[0]) {
+            for (i=0; i < boutonsDates.length; i++){
+                boutonsDates[i].value = "";
+            }
+        }
+        if (prixSejour[id]){
+            prixSejour[id].innerHTML = "0€ pour les nuits";
+        }
+    }
+}
+
+//fonction qui affiche les plages
+function afficherPlages(tabPlage, classe, tabMotif, type, id){
+    if (type !== "NI"){
+        if (type === "D"){
+            tabDispo[id] = tabPlage;
+            tabPrix[id] = tabMotif;
+            classeDispo[id] = classe;
+        }
+        else{
+            tabIndispo[id] = tabPlage;
+            tabRaison[id] = tabMotif;
+            classeIndispo[id] = classe;
+        }
+        for (i=0; i < tabPlage.length; i++){
+            if (document.getElementById(id + "," + tabPlage[i]) && document.getElementById(id + "," + tabPlage[i]).className !== "actif") {
+                
+                document.getElementById(id + "," + tabPlage[i]).className = classe;
+                if (classe === "disponible"){
+                    document.getElementById(id + "," + tabPlage[i]).title = "prix de la plage : " + tabMotif[i] + "€";
+                }
+                else if (classe === "indisponible"){
+                    document.getElementById(id + "," + tabPlage[i]).title = "motif d'indisponibilité : " + tabMotif[i];
+                }
+            }
+        }
+    }
+    else{
+        tabIndispo[id] = [];
+        tabRaison[id] = [];
+        classeIndispo[id] = [];
+    }
+    
+}
     numCalendrier = -1;
 </script>
 
