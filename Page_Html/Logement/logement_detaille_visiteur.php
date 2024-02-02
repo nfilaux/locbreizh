49af0da38da210e72475e428ca9c68f839dc0a5a
FILAUX NATHAN <nfilaux@mx21>
2024-02-02 14:38:40 +0100

    avis page visiteur (oubli)

--- Page_Html/Logement/logement_detaille_visiteur.php
+++ Page_Html/Logement/logement_detaille_visiteur.php
@@ -917,6 +917,67 @@ function afficherPlages(tabPlage, classe, tabMotif, type, id){
             <p><?php echo 'Adresse : ' . $info['numero_rue'] . ' ' . $info['nom_rue'] . ' ' . $info['ville'] ?></p>  
             
         </div>
+        <hr class="hr">
+        <!--Les avis-->
+        <?php 
+            
+            $stmt = $dbh->prepare('SELECT moyenne_avis
+            from locbreizh._logement
+            where id_logement = :logement;');
+            $stmt->bindParam(':logement', $_GET['logement']);
+            $stmt->execute();
+            $moyenne = $stmt->fetch();
+
+            $stmt = $dbh->prepare('SELECT contenu_avis, note_avis, nom, prenom, photo
+            from locbreizh._avis a
+            join locbreizh._compte c on a.auteur = c.id_compte
+            where a.logement = :logement
+            ORDER BY a.id_avis DESC;');
+            $stmt->bindParam(':logement', $_GET['logement']);
+            $stmt->execute();
+            $avis = $stmt->fetchAll();
+            ?>
+            <div class="titreAvis">
+                <h3 class="h3_avis">Avis</h3>
+                <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg" class="note_moyenne">
+                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path></svg></label>
+                <p class="sousTitreAvis"><?php echo $moyenne['moyenne_avis'];?> ⏺ <?php echo count($avis); ?> avis</p>
+            </div>
+
+            <div class="all-avis">
+            <?php
+            $nb_avis = 0;
+            foreach($avis as $avi){
+                $nb_avis++;
+                ?>
+                <div class="box-avis <?php if($nb_avis > 4){echo 'hidden';}?>">
+                    <div class="avis-box-space-between">
+                        <div class="header-box infoC">
+                            <img src="../Ressources/Images/<?php echo $avi['photo'];?>" alt="Image de profil" title="Photo">
+                            <div>
+                                <p><?php echo $avi['prenom'] . ' ' . $avi['nom'];?></p>
+                                <hr>
+                            </div>
+                        </div>
+                        <div class="header-box">
+                            <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg" class="star-solid" fill="#ffa723">
+                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path></svg>
+                            <p><?php echo $avi['note_avis'];?>/5</p>
+                        </div>
+                    </div>
+                    <p><?php echo $avi['contenu_avis'];?></p>
+                    <div class="avis-box-space-between">
+                        <a href="">Répondre au commentaire</a>
+                        <a href="">Signaler</a>
+                    </div>
+                </div>
+            <?php } ?>
+            </div>
+            <?php
+            
+            if($nb_avis > 4){?>
+                <div class="div_plus_avis"><button id="afficher-plus-avis">Afficher tous les avis (<?php echo count($avis) - 4;?>)</button></div>
+            <?php } ?>
     </main>
     <?php
         // appel du footer
