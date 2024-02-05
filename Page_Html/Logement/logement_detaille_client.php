fb48d4b6128b9c8d22545f5d9ff5e6e6d49434f6
FILAUX NATHAN <nfilaux@mx21>
2024-02-05 08:32:03 +0100

    responsive avis

--- Page_Html/Logement/logement_detaille_client.php
+++ Page_Html/Logement/logement_detaille_client.php
@@ -917,7 +917,7 @@ function afficherPlages(tabPlage, classe, tabMotif, type, id){
                         <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg" class="star-solid">
                         <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path></svg></label>
                     </div>
-
+                    
                     <textarea maxlength="499" placeholder="Rediger votre avis..." type="text" id="messageInput" name="contenu"></textarea>
                     <button id="sendButton">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 664 663">
@@ -925,7 +925,6 @@ function afficherPlages(tabPlage, classe, tabMotif, type, id){
                         <path stroke-linejoin="round" stroke-linecap="round" stroke-width="33.67" stroke="#6c6c6c" d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"></path>
                         </svg>
                     </button>
-
                 </form>
             </div>
             <?php } ?>
@@ -960,7 +959,7 @@ function afficherPlages(tabPlage, classe, tabMotif, type, id){
                 </div>
             <?php } ?>
             </div>
-            <?php
+            <?php 
             if($nb_avis == 0){ ?>
                 <p style="text-align : center">Aucun avis n'a encore été posté pour ce logement.</p>
             <?php }
